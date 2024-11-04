45a4c13a84e92fdf77dccdcd9365207ec26ffc47
batchlcwd <batchlcwd@gmail.com>
2024-11-04 13:18:23 +0530

    linkedlist practice _ linked list operations done

--- 3_linked_list/Node.java
+++ 3_linked_list/Node.java
@@ -1,11 +1,13 @@
-package singly;
+public class Node {
 
-class Node {
     public int data;
+
     public Node next;
 
+    //create: constructor
     public Node(int data) {
         this.data = data;
         this.next = null;
     }
+
 }
